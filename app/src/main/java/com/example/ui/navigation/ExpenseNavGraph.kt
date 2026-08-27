feb0a7abe6a1528a393c3876dee673e2a960b015
ashpakredmipad-max <ashpak.redmipad@gmail.com>
2026-08-27 18:16:27 +0530

    Update ExpenseNavGraph.kt

--- app/src/main/java/com/example/ui/navigation/ExpenseNavGraph.kt
+++ app/src/main/java/com/example/ui/navigation/ExpenseNavGraph.kt
@@ -82,7 +82,7 @@ fun ExpenseApp(viewModel: ExpenseViewModel, navController: NavHostController = r
                     title = {
                         Text(
                             text = currentScreen.title,
-                            fontWeight = FontWeight.Medium,
+                            fontWeight = FontWeight.bold,
                             fontSize = 22.sp,
                             letterSpacing = 0.5.sp
                         )
